b633351b87f125ccd45af14217b63a0a1dc19cae
Sparrow-33 <elhaoudihaytham2001@gmail.com>
2022-11-28 09:56:39 +0100

    JPA fixed

---
+++ src/main/java/DAO/Base.java
@@ -0,0 +1,38 @@
+package DAO;
+
+import JPA.Util.Util;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+
+import java.util.function.Consumer;
+
+public class Base<T> {
+    public static EntityManager entityManager = Util.getEntityManager();
+
+    public Boolean save(T t){
+        if(executeInsideTransaction(entityManager1 -> entityManager.persist(t))){
+            return true;
+        }   return false;
+    }
+    public Boolean update(T t){
+        if(executeInsideTransaction(entityManager1 -> entityManager.merge(t))){
+            return true;
+        }   return false;
+    }
+    // visitor dsp
+    public static Boolean executeInsideTransaction(Consumer<EntityManager> action) {
+        EntityTransaction tx = entityManager.getTransaction();
+        try {
+            tx.begin();
+            action.accept(entityManager);
+            tx.commit();
+            return true;
+        }
+        catch (RuntimeException e) {
+            tx.rollback();
+//            throw e;
+            return false;
+        }
+    }
+
+}
--- src/main/java/beans/Test.java
+++ src/main/java/beans/Test.java
@@ -1,7 +1,15 @@
 package beans;
 
+import jakarta.annotation.ManagedBean;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+
 import java.io.Serializable;
 
+@Named("testController")
+@RequestScoped
 public class Test implements Serializable {
-
+   public String moveToPage1() {
+        return "page1?faces-redirect = true";
+   }
 }
